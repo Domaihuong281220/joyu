74b89aac724d6936ff590b75372d70f2021b47a3
Lưu Lạc Ling <nguyenvantay061999@gmail.com>
2024-05-15 10:13:02 +0700

    TayNV-update

--- src/Component/index.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-// export { default as Footer } from "./Footer";
-
-// export { default as Banner } from "./HomePage/Banner";
-// export { default as Signature } from "./HomePage/Signature";
-// export { default as Special } from "./HomePage/Special";
