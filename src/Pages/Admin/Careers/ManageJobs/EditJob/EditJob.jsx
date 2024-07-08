8c72e728c8704e7a2aa09fbf3ea8853a91d9234e
Trình TN <115331941+trinhtn4322@users.noreply.github.com>
2024-07-08 11:31:56 +0700

    TrinhTN Edit hinh

--- src/Pages/Admin/Careers/ManageJobs/EditJob/EditJob.jsx
+++ src/Pages/Admin/Careers/ManageJobs/EditJob/EditJob.jsx
@@ -1,56 +1,61 @@
 /** @format */
 
 import React, { useState } from "react";
-// import { Input } from "@material-tailwind/react";
-import { Icon } from "@iconify/react";
 import { Input } from "antd";
-import { Select } from "@chakra-ui/react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import { toast } from "sonner";
 import { path } from "../../../../../utils/Constant";
-// import { isValidInputProduct } from "../../../../helpers/validInputs";
 
 const EditJob = () => {
   const navigate = useNavigate();
-
   const location = useLocation();
   let jobdetail = location.state;
 
-  // console.log(jobdetail);
   const [formData, setFormData] = useState({
     position: jobdetail.position,
     description: jobdetail.description,
     availability: jobdetail.availability,
-    // address: jobdetail.address,
     responsibility: jobdetail.responsibility,
     image: jobdetail.image,
   });
+
   const [image, setImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setImage(file);
+    }
+  };
 
   const handleEdit = async (id) => {
     const updateData = new FormData();
     updateData.append("position", formData.position);
     updateData.append("description", formData.description);
     updateData.append("availability", formData.availability);
-    // updateData.append("address", formData.address);
     updateData.append("responsibility", formData.responsibility);
+
     if (image) {
       updateData.append("image", image);
     } else {
-      updateData.append("image", formData.image); // Make sure the current image path is sent
+      updateData.append("image", formData.image);
     }
+
     await axios
-      .put(`${process.env.REACT_APP_SERVER_URL}/joyu/careers/${id}`, formData)
+      .put(`${process.env.REACT_APP_SERVER_URL}/joyu/careers/${id}`, updateData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
       .then((res) => {
         if (res.status === 200 || res.status === 201) {
           toast.success("Edit job successfully!");
-
           navigate("../" + path.JOBMANAGE);
         }
       })
       .catch((err) => {
-        toast.error("Edit job wrong: " + err.message);
+        toast.error("Edit job failed: " + err.message);
       });
   };
 
@@ -58,9 +63,8 @@ const EditJob = () => {
     <div className="">
       <div className="w-[90%] mx-auto h-auto bg-white shadow-xl rounded-lg p-1">
         <div className="flex p-2">
-          <p className="text-2xl"> EDIT JOB</p>
+          <p className="text-2xl">EDIT JOB</p>
         </div>
-        {/* Start Form Edit Product */}
         <div className="px-10 py-4 mx-auto w-[50%] ">
           <div className="w-full h-auto flex flex-col justify-start items-start gap-y-2 pb-6">
             <p className="text-lg">Position</p>
@@ -78,9 +82,7 @@ const EditJob = () => {
           <div className="w-full h-auto flex flex-col justify-start items-start gap-y-2 pb-6">
             <p className="text-lg">
               Description
-              <span className="text-[10px] text-red-500">
-                (Limit 500 characters)
-              </span>
+              <span className="text-[10px] text-red-500">(Limit 500 characters)</span>
             </p>
             <textarea
               className="w-full h-[300px] border-[1px] p-2"
@@ -98,13 +100,11 @@ const EditJob = () => {
           <div className="w-full h-auto flex flex-col justify-start items-start gap-y-2 pb-6">
             <p className="text-lg">
               Responsibility
-              <span className="text-[10px] text-red-500">
-                (Limit 500 characters)
-              </span>
+              <span className="text-[10px] text-red-500">(Limit 500 characters)</span>
             </p>
             <textarea
               className="w-full h-[300px] border-[1px] p-2"
-              placeholder="Description"
+              placeholder="Responsibility"
               maxLength="500"
               value={formData.responsibility}
               onChange={(e) =>
@@ -112,54 +112,43 @@ const EditJob = () => {
               }
             />
             <div className="text-right w-full text-sm text-gray-600">
-              {formData.description.length}/500
+              {formData.responsibility.length}/500
             </div>
           </div>
-          {/* <div className="w-full h-auto flex flex-col justify-start items-start gap-y-2 pb-6">
-            <p className="text-lg">Address</p>
-            <Input
-              type="text"
-              className="w-full h-auto border-[1px] p-2"
-              placeholder="Address"
-              defaultValue={jobdetail.address}
-              onChange={(e) =>
-                setFormData({ ...formData, address: e.target.value })
-              }
-            />
-          </div> */}
           <div className="w-full h-auto flex flex-col justify-start items-start gap-y-2 pb-6">
             <p className="text-lg">Availability</p>
             <input
-              className=" border-[1px] p-2"
-              placeholder="Subtitle"
+              className="border-[1px] p-2"
               type="checkbox"
-              // defaultValue={jobdetail.availability}
-              defaultChecked={jobdetail.availability === "false" ? false : true}
-              // checked={jobdetail.availability}
+              defaultChecked={jobdetail.availability === "true"}
               onChange={(e) => {
                 setFormData({
                   ...formData,
-                  availability: e.target.checked,
+                  availability: e.target.checked.toString(),
                 });
               }}
             />
           </div>
-          {/* <div className="w-full h-auto flex flex-col justify-start items-start gap-y-2 pb-6">
-            <p className="text-lg">Current Image</p>
-            {formData.image && (
-              <img
-                src={`${process.env.REACT_APP_SERVER_URL}/${formData.image}`}
-                alt="Product"
-                className="h-[200px] w-[200px] mb-4"
-              />
-            )}
+          <div className="w-full flex flex-col gap-y-2 pb-6">
+            <p className="text-lg">Edit Image</p>
+            <img
+              src={`${process.env.REACT_APP_SERVER_URL}/${formData.image}`}
+              alt="current job"
+            />
             <input
               type="file"
               accept="image/jpeg, image/png"
-              onChange={(e) => setImage(e.target.files[0])}
+              onChange={handleImageChange}
+              className="w-full"
             />
-          </div> */}
-
+            {image && (
+              <img
+                src={URL.createObjectURL(image)}
+                alt="New"
+                className="w-[200px] h-[200px] object-cover"
+              />
+            )}
+          </div>
           <div className="flex justify-center items-center gap-x-4">
             <button
               className="w-auto h-auto py-2 px-4 bg-slate-50 border-2 border-blue-300 rounded-lg hover:bg-slate-200 hover:shadow-lg"
@@ -167,18 +156,17 @@ const EditJob = () => {
                 navigate(-1);
               }}
             >
-              <p className="">Back</p>
+              <p>Back</p>
             </button>
 
             <button
-              className="w-auto h-auto py-2 px-4 bg-blue-300 border-2 border-blue-300 rounded-lg hover:bg-blue-500 hover:shadow-lg "
+              className="w-auto h-auto py-2 px-4 bg-blue-300 border-2 border-blue-300 rounded-lg hover:bg-blue-500 hover:shadow-lg"
               onClick={() => handleEdit(jobdetail._id)}
             >
-              <p className="">Save</p>
+              <p>Save</p>
             </button>
           </div>
         </div>
-        {/* End Form Edit Product */}
       </div>
     </div>
   );
